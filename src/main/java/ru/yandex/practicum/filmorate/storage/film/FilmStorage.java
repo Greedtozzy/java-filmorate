d38d06ad393ef1322a98b18d545aa559311bb83c
a.arkhipov <arkhipov.antosha@yandex.ru>
2023-06-06 16:40:05 +0300

    ТЗ-10 Ревью 1 Добавли пустые сроки. Проверка на GitHub не пропускала без них.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -2,15 +2,17 @@ package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public interface FilmStorage {
 
     Film getFilmById(int id);
+
     List<Film> getListAllFilms();
+
     Film addFilm(Film film);
+
     Film deleteFilm(Film film);
+
     Film updateFilm(Film film);
 }
