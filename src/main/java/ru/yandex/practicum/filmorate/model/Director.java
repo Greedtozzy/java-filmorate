691d3ea7576293aa90682cd9a164e371500373fc
a.arkhipov <arkhipov.antosha@yandex.ru>
2023-07-18 22:39:27 +0300

    add: model Director

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Director {
+    int id;
+    String name;
+}
